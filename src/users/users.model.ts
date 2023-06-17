3da2e71a070ccac1f72e84ab7f75e73d371628b2
Volodymyr Lysenko <lusiy321@gmail.com>
2023-06-17 20:57:35 +0300

    model

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -10,14 +10,26 @@ export class User extends Model<User, UserCreateAttrs>{
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number;
 
-    @Column({type: DataType.TEXT, unique: false, allowNull: false})
-    name: string;
+    @Column({type: DataType.TEXT})
+    firstName: string;
+
+    @Column({type: DataType.TEXT})
+    lastName: string;
     
     @Column({ type: DataType.STRING, unique: true, allowNull: false })
     email: string;
     
     @Column({ type: DataType.STRING, unique: true, allowNull: false })
     password: string;
+
+    @Column({ type: DataType.STRING, defaultValue: '+38000000000' })
+    phone: string;
+
+    @Column({ type: DataType.STRING, defaultValue: 'Kyiv' })
+    location: string;
+
+    @Column({ type: DataType.STRING, })
+    avatarURL: string;
     
     @Column({ type: DataType.ENUM('user', 'admin', 'moderator'), defaultValue: 'user', unique: true, allowNull: false })
     role: string;
