115605213a3b44ab0ac5338264017675bd724dfa
Volodymyr Lysenko <lusiy321@gmail.com>
2023-11-23 19:25:26 +0200

    new build

--- dist/orders/orders.controller.d.ts
+++ dist/orders/orders.controller.d.ts
@@ -1,6 +1,7 @@
 import { OrdersService } from './orders.service';
 import { TwilioService } from './twilio.service';
 import { Orders } from './order.model';
+import { CreateOrderDto } from './dto/create.order.dto';
 export declare class OrdersController {
     private readonly ordersService;
     private readonly twilioService;
@@ -8,6 +9,7 @@ export declare class OrdersController {
     constructor(ordersService: OrdersService, twilioService: TwilioService, ordersModel: Orders);
     findOrders(): Promise<Orders[]>;
     findIdOrders(id: string): Promise<Orders>;
+    create(user: CreateOrderDto): Promise<Orders>;
     sendVerificationCode(phoneNumber: string): Promise<any>;
     verifyBySms(code: string): Promise<Orders>;
     findPhoneUser(phone: string): Promise<Orders>;
